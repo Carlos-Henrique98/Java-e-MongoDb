d5a93ad21582eb296277c5179bb2240db108d737
Carlos Henrique <ichfs9108@gmail.com>
2022-02-28 23:42:20 -0300

    atualizando e deletando documento

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+import java.util.Date;
+
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 public class Principal {
 
@@ -11,7 +15,25 @@ public class Principal {
 		MongoDatabase bancoDeDados = cliente.getDatabase("test");
 		MongoCollection<Document> alunos = bancoDeDados.getCollection("alunos");
 		Document aluno = alunos.find().first();
+
 		System.out.println(aluno);
+
+		/*
+		 * Document novoAluno = new Document("nome", "João").append("data_nascimento",
+		 * new Date(2003, 10, 10)) .append("curso", new Document("nome",
+		 * "História")).append("notas", Arrays.asList(10, 9, 8)) .append("habilidades",
+		 * Arrays.asList(new Document().append("nome", "Inglês").append("nível",
+		 * "Básico"), new Document().append("nome", "Espanhol").append("nivel",
+		 * "Básico")));
+		 * 
+		 * alunos.insertOne(novoAluno);
+		 * 
+		 * 
+		 * alunos.updateOne(Filters.eq("nome", "Mariana"), new Document("$set", new
+		 * Document("nome", "Joao Silva")));
+		 */
+		alunos.deleteOne(Filters.eq("nome", "Joao Silva"));
+
 		cliente.close();
 	}
 
